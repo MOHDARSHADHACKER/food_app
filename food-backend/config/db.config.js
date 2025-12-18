1f4145434242cf50bf4c607ee70142fee8952bdd
Mohd Arshad <mohdarshad2583@gmail.com>
2025-12-18 22:16:10 +0530

    moved the backend to another repo

--- food-backend/config/db.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    // url: "mongodb://localhost:27017/gofood"
-    url: "mongodb+srv://mohdarshad2583_db_user:tj9haNN6PW29tmGn@cluster0.duwfhqc.mongodb.net/?appName=Cluster0"
-};
-
-
-
--- food-backend/routes/FoodItem.routes.js
+++
@@ -1,9 +0,0 @@
-module.exports = app => {
-    
-   const FoodController = require("../controller/FoodItem.controller")
-    const router = require("express").Router();
-
-    router.get("/food", FoodController.findAll);
-
-     app.use('/api/fooditems', router);
-};
