b1b7490390ef0ee3a6d54eb9d0d9b0503575bbf5
Mohd Arshad <mohdarshad2583@gmail.com>
2025-12-18 21:54:44 +0530

    updated db url

--- food-backend/config/db.config.js
+++ food-backend/config/db.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    url: "mongodb://localhost:27017/gofood"
+    // url: "mongodb://localhost:27017/gofood"
+    url: "mongodb+srv://mohdarshad2583_db_user:tj9haNN6PW29tmGn@cluster0.duwfhqc.mongodb.net/?appName=Cluster0"
 };
 
 
